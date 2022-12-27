bee806bfb52c49e737cfba45a0218575fdc085f5
Daniel Vitor de Souza <daniel.souza@primitecnologia.local>
2022-12-27 16:46:23 -0300

    configure email cams

---
+++ lib/modules/barcode_scanner/barcode_scanner_status.dart
@@ -0,0 +1,28 @@
+import 'package:camera/camera.dart';
+
+class BarcodeScannerStatus {
+  final bool isCameraAvailable;
+  final String error;
+  final String barcode;
+  final CameraController? cameraController;
+
+  BarcodeScannerStatus(
+      {this.isCameraAvailable = false,
+      this.error = "",
+      this.barcode = "",
+      this.cameraController});
+
+  factory BarcodeScannerStatus.available(CameraController controller) =>
+      BarcodeScannerStatus(
+          isCameraAvailable: true, cameraController: controller);
+
+  factory BarcodeScannerStatus.error(String message) =>
+      BarcodeScannerStatus(error: message);
+
+  factory BarcodeScannerStatus.barcode(String barcode) =>
+      BarcodeScannerStatus(error: barcode);
+
+  bool get showCamera => isCameraAvailable && error.isEmpty;
+  bool get hasError => error.isNotEmpty;
+  bool get hasBarcode => barcode.isNotEmpty;
+}
---
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class InsertBoletoPage extends StatelessWidget {
+  const InsertBoletoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Insert boleto page")),
+    );
+  }
+}
